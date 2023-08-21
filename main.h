bba3641bfeb95544992dfbc617b2eed421c8c6bc
Tess-Olere <omokpotessy@gmail.com>
2023-08-21 16:48:50 -0700

    main.h

--- main.h
+++ main.h
@@ -12,11 +12,11 @@
 #define F_PLUS 2
 #define F_ZERO 4
 #define F_HASH 8
-#define F_SPEACE 16
+#define F_SPACE 16
 
 /* SIZES */
-#define 5_LONG 2
-#define 5_SHORT 1
+#define S_LONG 2
+#define S_SHORT 1
 
 /**
  * struct fmt - struct op
@@ -26,7 +26,7 @@
 struct fmt
 {
 	char fmt;
-	int (*fn)(va_list, char [], int, int, int, int);
+	int (*fn)(va_list, char[], int, int, int, int);
 };
 
 /**
