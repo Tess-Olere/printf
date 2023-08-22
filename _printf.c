0bb8f799bc8da8f53d448d8cf43e0c4ac14ba11b
Tess-Olere <omokpotessy@gmail.com>
2023-08-21 18:11:15 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -12,7 +12,7 @@ int _printf(const char *format, ...)
 	int i, printed = 0, printed_chars = 0;
 	int flags, width, precision, size, buff_ind = 0;
 	va_list list;
-	char buffer[BUFFER_SIZE];
+	char buffer[BUFF_SIZE];
 
 	if (format ==  NULL)
 		return (-1);
@@ -45,7 +45,7 @@ int _printf(const char *format, ...)
 		}
 	}
 
-	printed_buffer(buffer, &buff_ind);
+	print_buffer(buffer, &buff_ind);
 
 	va_end(list);
 
